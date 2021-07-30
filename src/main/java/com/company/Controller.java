633108f5a46bcbc688f694ebeaaf2bd6548b7b3b
WMCobb00 <Billycobb00@gmail.com>
2021-07-30 17:58:02 -0400

    FXML and Controller class updates

--- src/main/java/com/company/Controller.java
+++ src/main/java/com/company/Controller.java
@@ -1,4 +1,28 @@
 package com.company;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    public SplitPane splitPane;
+    public MenuBar menuBar;
+    public ChoiceBox algorithms, mazesAndPatterns, speed;
+    public Button drive, addPOI, clearBoard, clearWallsAndPOIs, clearPath;
+    public GridPane gridPane;
+
+
+    @Override
+    public void initialize(URL idkYet, ResourceBundle stillDk){
+
+    }
 }
